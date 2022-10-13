98a5dc630082210db7d8e7b290f1637d037c7f00
SwarupParua <108712030+SwarupParua@users.noreply.github.com>
2022-10-13 12:41:45 +0530

    Add files via upload

---
+++ Question6.c
@@ -0,0 +1,12 @@
+/* Write a C program to find an ASCII value of a character. */
+
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("Enter a single character value is :");
+    scanf("%c",&c);
+    printf("ASCII code of the character value is :%d",c);
+
+    return 0;
+}
